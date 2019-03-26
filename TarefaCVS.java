85695a97aaca89c7918ecc48d330d9b61ecbd8ed
gtplayer001 <48951373+gtplayer001@users.noreply.github.com>
2019-03-26 13:28:50 +0100

    Add files via upload

---
+++ TarefaCVS.java
@@ -0,0 +1,25 @@
+package tarefacvs;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author A18YagoRM
+ */
+public class TarefaCVS {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce un numero enteiro:");
+        int numero = sc.nextInt();
+        
+        if(numero % 2 == 0){
+            System.out.println("PAR");
+        }else{
+            System.out.println("IMPAR");
+        }
+        
+    }
+    
+}
